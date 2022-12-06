df75eb895c6d62425c2ab3a48c05057e35ac5897
Jayse Weaver <jmweaver2@wisc.edu>
2022-12-05 19:16:27 -0600

    day 5

---
+++ day05/day05_p2.py
@@ -0,0 +1,64 @@
+# load file
+with open('input.txt') as f:
+    lines = f.readlines()
+
+# PART 2
+# init lists
+x = [[], [], [], [], [], [], [], [], []]
+
+instruction_start = 0
+for i in range(len(lines)):
+    line = list(lines[i][:-1])
+    if line == [] or line[1] == '1':  # we don't need this shit
+        continue
+    elif line[0] == 'm':
+        instruction_start = i
+        break
+    else:  # construct lists of elements
+        print(line)
+        if len(line) > 1:  # first column
+            if line[1] != ' ':
+                x[0].append(line[1])
+        if len(line) > 5:  # second column
+            if line[5] != ' ':
+                x[1].append(line[5])
+        if len(line) > 9:  # third column
+            if line[9] != ' ':
+                x[2].append(line[9])
+        if len(line) > 13:  # fourth column
+            if line[13] != ' ':
+                x[3].append(line[13])
+        if len(line) > 17:  # fifth column
+            if line[17] != ' ':
+                x[4].append(line[17])
+        if len(line) > 21:  # sixth column
+            if line[21] != ' ':
+                x[5].append(line[21])
+        if len(line) > 25:  # seventh column
+            if line[25] != ' ':
+                x[6].append(line[25])
+        if len(line) > 29:  # eighth column
+            if line[29] != ' ':
+                x[7].append(line[29])
+        if len(line) > 33:  # ninth column
+            if line[33] != ' ':
+                x[8].append(line[33])
+
+print('break')
+for i in range(len(x)):
+    x[i] = x[i][::-1]
+    print(x[i])
+
+print(' ')
+for i in range(instruction_start, len(lines)):
+    move_num = lines[i].split(' ')[1]
+    source = lines[i].split(' ')[3]
+    destination = lines[i].split(' ')[5]
+
+    temp = x[int(source)-1][-int(move_num):]  # removed strings
+    x[int(source) - 1] = x[int(source) - 1][:-int(move_num)]
+    for i in range(len(temp)):
+        x[int(destination) - 1].append(temp[i])
+
+for i in range(len(x)):
+    print(x[i])
